2f72bd0df408229374acfe551e1fee759e5a2242
v4kst1z <dcydane@gmail.com>
2020-05-01 21:23:32 +0800

    :construction_worker: Add Parser

---
+++ parser/src/ast.rs
@@ -0,0 +1,100 @@
+#[derive(Debug, PartialEq)]
+pub struct Program {
+    prog_name: String,
+    pub fun_decl: FunctionDecls,
+}
+
+pub type FunctionDecls = Vec<FunctionDecl>;
+
+#[derive(Debug, PartialEq, Default)]
+pub struct FunctionDecl {
+    pub function_name: String,
+    pub paras: Vec<Expression>,
+    pub body: Statements,
+}
+
+pub type Statements = Vec<Statment>;
+
+#[derive(Debug, PartialEq)]
+pub enum Statment {
+    VarAssignment{
+        target: Box<Expression>,
+        value: Box<Expression>,
+    },
+    PointerAssignment {
+        target: Box<Expression>,
+        value: Box<Expression>,
+    },
+    LocalDecl {
+        names: Vec<Expression>,
+    },
+    Output {
+        target: Expression,
+    },
+    While {
+        cond: Expression,
+        body: Statements,
+    },
+    Return { value: Expression },
+    If {
+        cond: Expression,
+        then: Statements,
+        orelse: Option<Statements>,
+    },
+}
+
+pub type Expressions = Vec<Expression>;
+
+pub enum Expression {
+    Malloc,
+    Input,
+    Null,
+    Number {
+        value: Number,
+    },
+    Identifier {
+        name: String,
+    },
+    Binop {
+        a: Box<Expression>,
+        op: Operator,
+        b: Box<Expression>,
+    },
+    Unop {
+        op: UnaryOperator,
+        a: Box<Expression>,
+    },
+    Call {
+        function_name: Box<Expression>,
+        args: Vec<Expression>,
+    },
+    PointerInvocation {
+        function_name: Box<Expression>,
+        args: Vec<Expression>,
+    },
+}
+
+/*
+ * Operator precedence:
+ * & *
+ * * /
+ * + -
+ * = >
+ */
+
+#[derive(Debug, PartialEq)]
+pub enum Operator {
+    Add,
+    Sub,
+    Mult,
+    Div,
+    Gt,
+    Eq,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum UnaryOperator {
+    Minus,
+    Pointer,
+    Dereference,
+}
---
+++ parser/src/parser.rs
@@ -0,0 +1,367 @@
+use lexer::token::Token;
+use std::fmt::Debug;
+use std::io::Error;
+use crate::ast::{Statements, Expression, Program, FunctionDecl, Parameter, Operator, UnaryOperator};
+use lexer::token::Token::{Ident, Rparen, Comma};
+use crate::ast::Statment::{VarAssignment, LocalDecl, Output, Return, While, If};
+use crate::ast::Expression::{Malloc, Input, Null, Number, Identifier, Binop, Unop};
+
+#[derive(Clone, PartialEq)]
+pub struct Parser<T: Iterator<Item = Token> + Debug + Clone> {
+    token: T,
+    tok0: Option<Token>,
+    tok1: Option<Token>,
+    pub result: Program
+}
+
+impl<T> Parser<T>
+    where
+        T: Iterator<Item = Token> + Debug + Clone {
+    pub fn new(input: T) -> Self {
+        let mut parser = Parser {
+            token: input,
+            tok0: None,
+            tok1: None,
+            result: Program { prog_name: "".to_string(),  fun_decl: vec![] }
+        };
+        parser.next_token();
+        parser.next_token();
+
+        parser
+    }
+
+    fn parse_fun_args(&mut self) -> Result<Vec<Expression>, Error> {
+        let mut args_out = vec![];
+        self.next_token(); //consume (
+        loop {
+            if self.tok0 = Some(Comma) {
+                self.next_token(); // consume Comma
+            } else if self.tok0 = Some(Rparen) {
+                self.next_token(); // consume Rparen
+                break;
+            }
+
+            let tmp_expr = self.parse_expr()?;
+            args_out.push(tmp_expr);
+        }
+        Ok(args_out)
+    }
+
+    fn expr(&mut self) -> Result<Expression, Error> {
+        match self.tok0.clone() {
+            Some(Token::Alloc) => {
+                self.next_token();
+                Ok(Malloc)
+            }
+            Some(Token::Input) => {
+                self.next_token();
+                Ok(Input)
+            }
+            Some(Token::Null) => {
+                self.next_token();
+                Ok(Null)
+            }
+            Some(Token::Number(num)) => {
+                self.next_token();
+                Ok(Number {
+                    value: num,
+                })
+            }
+            Some(Token::Ident(name)) => {
+                if let Some(Token::Lparen) = self.tok1 {
+                    let call_fun = Box::new(Expression::Identifier {name});
+                    self.next_token();   //consume function name
+                    let args = self.parse_fun_args()?;
+                    //parse function arguments
+                    Ok(Expression::Call {
+                        function_name: call_fun,
+                        args
+                    })
+                } else {
+                    Ok(Identifier {
+                        name,
+                    },)
+                }
+            }
+            Some(Token::Lparen) if self.tok1 = Some(Token::Mult) => {
+                //TODO
+            }
+            _ => panic!("Err parse expr!")
+        }
+    }
+
+    fn unary_expr(&mut self) -> Result<Expression, Error> {
+        match self.tok0 {
+            Some(Token::Mult) => {
+                self.next_token(); //consume *
+                let expr = self.unary_expr()?;
+                Ok(Unop {
+                    op: UnaryOperator::Dereference,
+                    a: Box::new(expr)
+                })
+            }
+            Some(Token::Ampersand) => {
+                self.next_token(); //consume &
+                let expr = self.unary_expr()?;
+                Ok(Unop {
+                    op: UnaryOperator::Pointer,
+                    a: Box::new(expr)
+                })
+            }
+            Some(Token::Sub) => {
+                self.next_token(); //consume -
+                let expr = self.unary_expr()?;
+                Ok(Unop {
+                    op: UnaryOperator::Minus,
+                    a: Box::new(expr)
+                })
+            }
+            _ => self.expr(),
+        }
+    }
+
+    fn multiplicative_expr(&mut self) -> Result<Expression, Error> {
+        let mut expr = self.unary_expr()?;
+        loop {
+            let operator = match self.tok0 {
+                Some(Token::Mult) => {
+                    self.next_token(); //consume *
+                    Operator::Mult
+                }
+                Some(Token::Div) => {
+                    self.next_token(); //consume /
+                    Operator::Div
+                }
+                _ => break,
+            };
+            let right = Box::new(self.additive_expr()?);
+            expr = Binop {
+                a: Box::new(expr),
+                op: operator,
+                b: right
+            }
+        }
+        Ok(expr)
+    }
+
+    fn additive_expr(&mut self) -> Result<Expression, Error> {
+        let mut expr = self.multiplicative_expr()?;
+        loop {
+            let operator = match self.tok0 {
+                Some(Token::Plus) => {
+                    self.next_token(); //consume +
+                    Operator::Add
+                }
+                Some(Token::Sub) => {
+                    self.next_token(); //consume -
+                    Operator::Sub
+                }
+                _ => break,
+            };
+            let right = Box::new(self.additive_expr()?);
+            expr = Binop {
+                a: Box::new(expr),
+                op: operator,
+                b: right
+            }
+        }
+        Ok(expr)
+    }
+
+    fn relational_expr(&mut self) -> Result<Expression, Error> {
+        let mut expr = self.additive_expr()?;
+        loop {
+            let operator = match self.tok0 {
+                Some(Token::Great) => {
+                    self.next_token(); //consume >
+                    Operator::Gt
+                }
+                Some(Token::Equal) => {
+                    self.next_token(); //consume ==
+                    Operator::Eq
+                }
+                _ => break,
+            };
+            let right = Box::new(self.additive_expr()?);
+            expr = Binop {
+                a: Box::new(expr),
+                op: operator,
+                b: right
+            }
+        }
+        Ok(expr)
+    }
+
+    fn parse_expr(&mut self) -> Result<Expression, Error> {
+        self.relational_expr()
+    }
+
+    fn parse_decl_var(&mut self) -> Result<Vec<Expression>, Error> {
+        let mut decl_vars = vec![];
+
+        loop {
+            if self.tok0 == Some(Token::Semi) {
+                break;
+            } else if self.tok0 == Some(Token::Comma) {
+                self.next_token();
+            }
+
+            if let Some(Ident(decl_var_name)) = self.tok0.clone() {
+                decl_vars.push(Expression::Identifier { name: decl_var_name });
+                self.next_token();
+            }
+
+        }
+
+        Ok(decl_vars)
+    }
+
+    fn parse_cond_expr(&mut self) -> Result<Expression, Error> {
+        self.next_token(); //consume (
+        let cond_expr = self.parse_expr()?;
+        self.next_token(); // consume )
+
+        Ok(cond_expr)
+    }
+
+    fn parse_block(&mut self, ) -> Result<Vec<Statement>, Error> {
+        let mut stmts = vec![];
+        self.next_token(); // consume {
+        loop {
+            if self.tok0 == Some(Token::Rcurly) {
+                self.next_token();
+                break;
+            }
+            stmts.push(self.parse_stmt()?);
+        }
+        Ok(stmts)
+    }
+
+
+    fn parse_stmt(&mut self) -> Result<Statement, Error> {
+        match self.tok0.clone() {
+            Some(Ident(var_name)) => {
+                self.next_token();
+                if let Some(Token::Assign) = self.tok0.clone() {
+                    let value = self.parse_expr()?;
+                    self.next_token(); //consume ;
+                    Ok(VarAssignment {
+                        target: Box::new(Expression::Identifier {name: var_name}),
+                        value: Box::new(value)
+                    })
+                } else {
+                    panic!("Err ident~~")
+                }
+            }
+            Some(Token::Var)  => {
+                self.next_token();
+                let names = self.parse_decl_var()?;
+                self.next_token(); //consume ;
+                Ok(LocalDecl {
+                    names
+                })
+            }
+            Some(Token::Output) => {
+                self.next_token();
+                let out_expr = self.parse_expr()?;
+                self.next_token(); //consume ;
+                Ok(Output {
+                    target: out_expr,
+                })
+            }
+            Some(Token::Return) => {
+                self.next_token();
+                let ret_expr = self.parse_expr()?;
+                self.next_token(); //consume ;
+                Ok(Return {
+                    value: ret_expr,
+                })
+            }
+            Some(Token::While) => {
+                self.next_token();
+                let cond_expr = self.parse_cond_expr()?;
+                //self.next_token(); // consume {
+                let mut out_stmts = self.parse_block()?;
+                Ok(While {
+                    cond: cond_expr,
+                    body: out_stmts,
+                })
+            }
+            Some(Token::If) => {
+                self.next_token();
+                let cond_expr = self.parse_cond_expr()?;
+                let then_stmts = self.parse_block()?;
+                if let Some(Token::Else) = self.tok0 {
+                    self.next_token(); //consume else
+                    let else_stmts = self.parse_block()?;
+                    Ok(If {
+                        cond: cond_expr,
+                        then: then_stmts,
+                        orelse: Some(else_stmts),
+                    },)
+                } else {
+                    Ok(If {
+                        cond: cond_expr,
+                        then: then_stmts,
+                        orelse: None,
+                    },)
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn parse_paras(&mut self) -> Result<Vec<Expression>, Error> {
+        let mut paras = vec![];
+        self.next_token(); // consume Lparen
+        loop {
+            if self.tok0 = Some(Comma) {
+                self.next_token(); // consume Comma
+            } else if self.tok0 = Some(Rparen) {
+                self.next_token(); // consume Rparen
+                break;
+            }
+
+            if let Some(Ident(para_name)) = self.tok0.clone() {
+                paras.push(Expression::Identifier { name: para_name });
+                self.next_token(); // consume para
+            }
+        }
+        Ok(paras)
+    }
+
+    fn parse_func(&mut self) -> Result<FunctionDecl, Error> {
+        let mut tmp_func = FunctionDecl::default();
+        if let Some(Ident(fun_name)) = self.tok0.clone() {
+            tmp_func.function_name = fun_name;
+            self.next_token(); //consume function name
+        }
+        tmp_func.paras = self.parse_paras()?;
+        tmp_func.body = self.parse_stmt()?;
+
+        Ok(tmp_func)
+    }
+
+    fn parse_prog(&mut self)  {
+        loop {
+            match self.tok0.clone() {
+                Some(tok) => {
+                    let tmp_func = self.parse_func()?;
+                    self.result.fun_decl.push(tmp_func);
+                },
+                _ => {
+                    break
+                },
+            }
+        }
+
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        let tmp_token = self.tok0.clone();
+        self.tok0 = self.tok1.clone();
+        self.tok1 = self.token.next();
+
+        tmp_token
+    }
+}
